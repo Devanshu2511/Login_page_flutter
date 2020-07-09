87fa46b646a6e917ea1a4ceb2f3818ec7d20acf0
Devanshu2511 <65583643+Devanshu2511@users.noreply.github.com>
2020-07-09 14:56:54 +0530

    Add files via upload

---
+++ screens/home/home.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/services/auth.dart';
+
+class Home extends StatelessWidget {
+
+  final AuthService _auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        backgroundColor: Colors.green[50],
+        appBar: AppBar(
+          title: Text('My App'),
+          backgroundColor: Colors.green[400],
+          elevation: 0.0,
+          actions: <Widget>[
+            FlatButton.icon(
+              icon: Icon(Icons.person),
+              label: Text('logout'),
+              onPressed: () async {
+                await _auth.signOut();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
